3d3f3270656abf241dfc0ce16e15e972be7673d8
Tommsy64 <thomas.pakh@gmail.com>
2013-12-05 08:22:00 -0500

    Added a solver that uses a Sudoku board to solve them

--- src/io/github/programmingclub/Main.java
+++ src/io/github/programmingclub/Main.java
@@ -1,16 +1,17 @@
 package io.github.programmingclub;
 
+import java.io.IOException;
+
 public class Main {
 
 	private static class Info {
 		private static String VERSION = "0.0.1";
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("-------------Initiating Sudoku Solver v"
 				+ Info.VERSION + "-------------\n");
-		
-		
+
 		SudokuBoard test = new SudokuBoard();
 	}
 }
